85d6d686a30d83b9099c322046cc250a32c72f22
pisunkovi <starikbanana@gmail.com>
2026-08-31 16:57:07 +0300

    Update PlayerEntityRendererMixin.java

--- src/main/java/com/wardrobe/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/wardrobe/mixin/PlayerEntityRendererMixin.java
@@ -1,12 +1,14 @@
-package com.wardrobe.mixin;
-
-import com.wardrobe.auth.ElyAuthManager;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-@Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin {
-    // Усунення відсікання плаща при надягнених елітрах для синхронного рендеру
+{
+  "required": true,
+  "package": "com.wardrobe.mixin",
+  "compatibilityLevel": "JAVA_21",
+  "client": [
+    "TitleScreenMixin",
+    "GameMenuScreenMixin",
+    "CapeFeatureRendererMixin",
+    "ElytraFeatureRendererMixin"
+  ],
+  "injectors": {
+    "defaultRequire": 1
+  }
 }
